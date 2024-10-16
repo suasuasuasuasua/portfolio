156fcab58cef055bd308e84b3d34a1eef9b91f09
Justin Hoang <j124.dev@proton.me>
2024-10-16 17:56:46 -0600

    feat(app/*/page.tsx): Added dummy pages
    No content yet, also there's weird shifting going on...

---
+++ app/blog/page.tsx
@@ -0,0 +1,8 @@
+export default function BlogPage() {
+  return (
+    <>
+      <h1>Blog page</h1>
+      <p>Hello</p>
+    </>
+  );
+}
---
+++ app/portfolio/page.tsx
@@ -0,0 +1,8 @@
+export default function PortfolioPage() {
+  return (
+    <>
+      <h1>Portfolio page</h1>
+      <p>Hello</p>
+    </>
+  );
+}
