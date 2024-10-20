683cdda4c52e69121aa8c069096a82e4ecc98a97
Justin Hoang <j124.dev@proton.me>
2024-10-20 14:07:43 -0600

    feat(app/page.tsx): added spotify widget component
    displays the current listen (will have to update manually i think?)

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,20 @@
+import { Spotify } from "react-spotify-embed";
+
 export default function Home() {
   return (
     <div className="flex flex-col mx-10">
+      {/* Leading Title */}
       <h1 className="font-bold">Welcome to my website!</h1>
+
+      {/* Current */}
+      <div className="flex flex-row my-10">
+        {/* Current Listen */}
+        <div className="flex flex-col">
+          <p className="font-semibold">Current Listen</p>
+          <Spotify link="https://open.spotify.com/album/4eguh1dJUXRh0IMiLKRwab" />
+        </div>
+        {/* TODO: Current Book */}
+      </div>
     </div>
   );
 }
