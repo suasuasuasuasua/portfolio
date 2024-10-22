34a3965985539704ad86fbbe110d2f12da129758
Justin Hoang <j124.dev@proton.me>
2024-10-22 10:17:53 -0600

    feat(app/tech/page.tsx): added general software that i use
    looks really good so far! one thing is just icon placement but idk

--- app/tech/page.tsx
+++ app/tech/page.tsx
@@ -1,9 +1,11 @@
 import computers from "@/app/tech/computers";
+import { devSoftware, cliSoftware, genSoftware } from "@/app/tech/software";
 import Computer from "@/components/tech/computer";
+import { Software, CLISoftware } from "@/components/tech/software";
 
 export default function TechPage() {
   return (
-    <div className="mx-4 flex flex-col">
+    <div className="mx-auto flex flex-row space-x-10">
       {/* Computers Section */}
       <div>
         <p className="text-xl font-bold">Computers</p>
@@ -15,6 +17,46 @@ export default function TechPage() {
             </li>
           ))}
         </ul>
+      </div>
+      {/* Software Section */}
+      <div>
+        {/* Group the Development and CLi */}
+        <div>
+          {/* Development Section */}
+          <div>
+            <p className="text-lg font-bold">Development</p>
+            <ul>
+              {Object.entries(devSoftware).map(([key, value]) => (
+                <li key={key} className="mb-2">
+                  <Software software={value} />
+                </li>
+              ))}
+            </ul>
+          </div>
+          {/* CLI Development Section */}
+          <div>
+            <p className="text-lg font-bold">CLI</p>
+            <ul>
+              {Object.entries(cliSoftware).map(([key, value]) => (
+                <li key={key} className="mb-4">
+                  <CLISoftware software={value} />
+                </li>
+              ))}
+            </ul>
+          </div>
+        </div>
+
+        {/* General Software */}
+        <div>
+          <p className="text-lg font-bold">General</p>
+          <ul>
+            {Object.entries(genSoftware).map(([key, value]) => (
+              <li key={key} className="mb-4">
+                <Software software={value} />
+              </li>
+            ))}
+          </ul>
+        </div>
       </div>
     </div>
   );
