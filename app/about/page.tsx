cef1e3c5781aa68512c4112abb5f158e16e1f50c
Justin Hoang <j124.dev@proton.me>
2024-10-22 15:31:40 -0600

    feat(app/about/page.tsx): filled out the about page route
    going to add contact information next

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,5 +1,21 @@
-import Placeholder from "@/components/placeholder";
+import { faq } from "@/app/about/faq";
+import Answer from "@/components/about/answer";
+import Question from "@/components/about/question";
 
 export default function AboutPage() {
-  return <Placeholder />;
+  return (
+    /* FAQ */
+    <div className="mx-auto mb-8 w-11/12 md:w-8/12 lg:w-6/12">
+      <ul className="space-y-2">
+        {Object.entries(faq).map(([key, value]) => (
+          <li key={key}>
+            <div className="">
+              <Question question={value.question} />
+              <Answer answer={value.answer} />
+            </div>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
 }
