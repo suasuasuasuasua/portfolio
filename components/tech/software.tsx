9a56acb9392ebc3d6897d5057449ddd0ed34889b
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2024-12-19 18:55:12 -0700

    fix: added small margin between name and icon for software  (#159)
    * fix: increased padding for software platforms under tech
    
    literally 1 to 1.5 padding
    
    * style: added icons for homebrew and nix
    
    the tech page is a bit more cool now
    
    * fix: added small margin between name and icon for software
    
    why did this take so long

--- components/tech/software.tsx
+++ components/tech/software.tsx
@@ -8,8 +8,8 @@ export function Software({
     <div>
       {/* Name */}
       <div className="flex flex-row md:flex-col">
-        <span className="inline-flex space-x-1">
-          {Icon ? <Icon className="mt-1" /> : ""}
+        <span className="mr-1 inline-flex space-x-1.5 sm:mr-0">
+          {Icon ? <Icon className="mt-1 sm:mt-0" /> : ""}
           <HighlightedLink link={link} highlight="text-blue-400">
             {name}
           </HighlightedLink>
