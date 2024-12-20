31fc109fff8ab41185370b86193ecbb94116f97d
Justin Hoang <72476123+suasuasuasuasua@users.noreply.github.com>
2024-12-19 23:48:39 -0700

    fix: fixed height offset for software icons (#168)
    testing but forgot to remove this change!

--- components/tech/software.tsx
+++ components/tech/software.tsx
@@ -9,13 +9,13 @@ export function Software({
       {/* Name */}
       <div className="flex flex-row md:flex-col">
         <span className="mr-1 inline-flex space-x-1.5 sm:mr-0">
-          {Icon ? <Icon className="mt-1 sm:mt-0" /> : ""}
+          {Icon ? <Icon className="mt-1" /> : ""}
           <HighlightedLink link={link} highlight="text-blue-400">
             {name}
           </HighlightedLink>
         </span>
         {/* Platform */}
-        <ul className="mt-1 inline-flex space-x-1.5">
+        <ul className="mt-1 inline-flex space-x-1.5 sm:mt-0">
           {platforms.map((Platform, i) => (
             <li key={i}>{<Platform />}</li>
           ))}
